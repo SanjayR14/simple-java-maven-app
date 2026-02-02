82fbd893f4245ee7a9fd7a8146910fb3a6951e5f
Sanjay R <122106271+SanjayR14@users.noreply.github.com>
2026-02-02 20:28:37 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -9,7 +9,7 @@ public class App {
 
     public static void main(String[] args) {
         riskyMethod();
-        System.out.println("Hello World!");
+        System.out.println("Hello CI!");
     }
 
     // ✅ REQUIRED BY AppTest.java
